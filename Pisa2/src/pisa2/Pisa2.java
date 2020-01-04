0a93bddf7688833b369419f9a96d7d0430053358
rafael <rafael@rafael-desktop>
2020-01-03 22:51:13 -0600

    Guardar

---
+++ Pisa2/src/pisa2/Pisa2.java
@@ -0,0 +1,12 @@
+
+package pisa2;
+
+import javax.swing.JOptionPane;
+
+public class Pisa2 {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "Se venden pizzas");
+    }
+    
+}
